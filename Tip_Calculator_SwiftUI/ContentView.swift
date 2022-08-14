d8e3260a8c269cfe6416d7e01f3326b7e8702554
Elliott Larsen <elliottlrsn@gmail.com>
2022-08-14 12:54:11 -0600

    App complete.  Review the process and properties.

--- Tip_Calculator_SwiftUI/ContentView.swift
+++ Tip_Calculator_SwiftUI/ContentView.swift
@@ -8,10 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    @FocusState private var amountIsFocused: Bool
     @State private var checkAmount = 0.0
-    @State private var numberOfPeople = 2
+    @State private var numberOfPeople = 0
     @State private var tipPercentage = 20
     let tipPercentages = [0, 10, 15, 20, 25]
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 1)
+        let tipSelection = Double(tipPercentage)
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
     var body: some View {
         NavigationView{
             Form {
@@ -26,6 +36,7 @@ struct ContentView: View {
                     }
                 } header: {
                     Text("Enter the total bill and number of people")
+                .focused($amountIsFocused)
                 }
                 
                 Section {
@@ -40,11 +51,20 @@ struct ContentView: View {
                 }
                 
                 Section {
-                    Text(checkAmount, format: .currency(code:
+                    Text(totalPerPerson, format: .currency(code:
                                                             Locale.current.currencyCode ?? "USD"))
                 }
             }
             .navigationTitle("Tip Calculator")
+            .toolbar {
+              ToolbarItemGroup(placement: .keyboard) {
+                  Spacer()
+                Button("Done") {
+                  amountIsFocused = false
+            }
+                  
+              }
+            }
         }
     }
 }
