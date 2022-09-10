ba17a8d407d22e44d301e300bc4d1697e2b762b4
Elliott Larsen <elliottlrsn@gmail.com>
2022-09-09 23:49:24 -0600

    VStack testing.

--- Tip_Calculator_SwiftUI/ContentView.swift
+++ Tip_Calculator_SwiftUI/ContentView.swift
@@ -24,46 +24,47 @@ struct ContentView: View {
     }
     var body: some View {
         NavigationView{
-            Form {
-                Section {
-                    TextField("Amount", value: $checkAmount,
-                              format: .currency(code: Locale.current.currencyCode ?? "USD")).keyboardType(.decimalPad)
+            VStack {
+                Form {
+                    Section {
+                        TextField("Amount", value: $checkAmount,
+                                  format: .currency(code: Locale.current.currencyCode ?? "USD")).keyboardType(.decimalPad)
+                        
+                        Picker("Number of People", selection: $numberOfPeople) {
+                            ForEach(1 ..< 100) {
+                                Text("\($0) people")
+                            }
+                        }
+                    } header: {
+                        Text("Enter the total bill and number of people")
+                            .focused($amountIsFocused)
+                    }
                     
-                    Picker("Number of People", selection: $numberOfPeople) {
-                        ForEach(1 ..< 100) {
-                            Text("\($0) people")
+                    Section {
+                        Picker("Tip percentage", selection: $tipPercentage) {
+                            ForEach(tipPercentages, id: \.self) {
+                                Text($0, format: .percent)
+                            }
+                        }
+                        .pickerStyle(.segmented)
+                    } header: {
+                        Text("How much tip would you like to leave?")
+                    }
+                    
+                    Section {
+                        Text(totalPerPerson, format: .currency(code:
+                                                                Locale.current.currencyCode ?? "USD"))
+                    }
+                }
+                .navigationTitle("Tip Calculator")
+                .toolbar {
+                    ToolbarItemGroup(placement: .keyboard) {
+                        Spacer()
+                        Button("Done") {
+                            amountIsFocused = false
                         }
                     }
-                } header: {
-                    Text("Enter the total bill and number of people")
-                .focused($amountIsFocused)
                 }
-                
-                Section {
-                    Picker("Tip percentage", selection: $tipPercentage) {
-                        ForEach(tipPercentages, id: \.self) {
-                            Text($0, format: .percent)
-                        }
-                    }
-                    .pickerStyle(.segmented)
-                } header: {
-                    Text("How much tip would you like to leave?")
-                }
-                
-                Section {
-                    Text(totalPerPerson, format: .currency(code:
-                                                            Locale.current.currencyCode ?? "USD"))
-                }
-            }
-            .navigationTitle("Tip Calculator")
-            .toolbar {
-              ToolbarItemGroup(placement: .keyboard) {
-                  Spacer()
-                Button("Done") {
-                  amountIsFocused = false
-            }
-                  
-              }
             }
         }
     }
